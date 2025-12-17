a576902019603ead4a02204948e903909a03bb6b
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-12-17 11:42:53 +0530

    [Deepanshu]: added WelcomeToBridgelabz program

---
+++ gcr-codebase/program-prerequisite/WelcomeToBridgelabz.java
@@ -0,0 +1,8 @@
+public class WelcomeToBridgelabz {
+    public static void main(String[] args) {
+        printWelcomeMessage();
+    }
+    private static void printWelcomeMessage() {
+        System.out.println("Welcome to Bridgelabz");
+    }
+}
