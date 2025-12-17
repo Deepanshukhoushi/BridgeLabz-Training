fe8e2e51593a7880d554cb680bdee4f31a39e5ee
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-12-17 15:21:24 +0530

    [Deepanshu]: added count average program

---
+++ gcr-codebase/programming-elements/Level 1/CountAge.java
@@ -0,0 +1,16 @@
+//Creating a class with name CountAge 
+public class CountAge{
+    public static void main(String[] args){
+	//creating a variable name birthyear to assign the value of birth year
+	int birthyear = 2000;
+
+	//creating a variable name currentyear to assigh the value of current year
+	int currentyear = 2024;
+
+	//creating variable name age to count the age
+	int age = currentyear - birthyear;
+	
+	//Display the results
+	System.out.println("Harry's current age is: " + age);
+    }
+}
