8cda464898b19607c2f465a8d5bdacfb2184d545
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-12-18 16:48:20 +0530

    [Deepanshu]: added Sum of n natural numbers using for loop and compare

---
+++ gcr-codebase/control-flows/Level 1/SumOfNaturalNumFor.java
@@ -0,0 +1,24 @@
+// importing Scanner to take user input and creating a class called SumOfNaturalNumbers
+import java.util.Scanner;
+public class SumOfNaturalNumFor{
+    public static void main(String[] args){
+	//taking input from user
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter a number: ");
+	int number = input.nextInt();
+	
+	//calculating the sum of n natural numbers 
+	int total1 = (number*(number+1))/2;
+	int total2 = 0;
+	
+	// calculating the sum of n natural numbers using for loop
+	for (int num = number; num>=0; num--){
+	    total2+=num;
+	}
+
+	//displaying the output if both sum are equal
+	if (total1 == total2){
+	    System.out.println("Both results matched, the total is " + total1);
+	}
+    }
+}
