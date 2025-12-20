c21cd9641d5771d1efd34348ab8c494f8d9959cc
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-12-20 10:18:24 +0530

    [Deepanshu]: added digit count using array program

---
+++ gcr-codebase/arrays/Level 2/DigitCount.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class DigitCount {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        number = Math.abs(number);
+        if (number == 0) {
+            System.out.println("Digit 0 occurs 1 time");
+            return;
+        }
+
+        int temp = number;
+        int count = 0;
+
+        while (temp != 0) {
+            count++;
+            temp /= 10;
+        }
+
+        int[] digits = new int[count];
+
+        temp = number;
+        for (int i = 0; i < count; i++) {
+            digits[i] = temp % 10;
+            temp /= 10;
+        }
+
+        int[] frequency = new int[10];
+        for (int i = 0; i < count; i++) {
+            frequency[digits[i]]++;
+        }
+
+        System.out.println("\nDigit Frequency:");
+        for (int i = 0; i < 10; i++) {
+            if (frequency[i] > 0) {
+                System.out.println("Digit " + i + " occurs " + frequency[i] + " time(s)");
+            }
+        }
+
+        input.close();
+    }
+}
