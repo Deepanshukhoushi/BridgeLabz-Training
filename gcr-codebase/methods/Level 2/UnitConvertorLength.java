ecd51eff013904c7fb77481ee309d1a8f69bad4d
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-12-21 21:28:06 +0530

    [Deepanshu]: added unit converter methods for lengths program

---
+++ gcr-codebase/methods/Level 2/UnitConvertorLength.java
@@ -0,0 +1,32 @@
+public class UnitConvertorLength {
+
+    // Yards to Feet
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    // Feet to Yards
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    // Meters to Inches
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    // Inches to Meters
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    // Inches to Centimeters
+    public static double convertInchesToCentimeters(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+}
