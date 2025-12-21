3ecebf52a6512412603388c40ea9e2a25df7579a
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-12-21 14:29:01 +0530

    [Deepanshu]: added generate random numbers and find average, minimum and maximum of numbers program

---
+++ gcr-codebase/methods/Level 2/RandomNumbers.java
@@ -0,0 +1,42 @@
+public class RandomNumbers {
+
+    // Method to generate 4-digit random numbers
+    static int[] generate4DigitRandomArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int) (Math.random() * 9000) + 1000;
+        }
+        return arr;
+    }
+
+    // Method to find average, min and max
+    static double[] findAverageMinMax(int[] numbers) {
+        int min = numbers[0];
+        int max = numbers[0];
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        double average = (double) sum / numbers.length;
+        return new double[]{average, min, max};
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = generate4DigitRandomArray(5);
+
+        System.out.print("Random Numbers: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        double[] result = findAverageMinMax(numbers);
+
+        System.out.println("\nAverage = " + result[0]);
+        System.out.println("Minimum = " + (int) result[1]);
+        System.out.println("Maximum = " + (int) result[2]);
+    }
+}
