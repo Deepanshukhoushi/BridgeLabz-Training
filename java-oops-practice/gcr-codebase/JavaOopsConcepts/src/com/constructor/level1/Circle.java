12b8116d7c77d437b782dbb2508fc260e9ffbb35
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-12-28 14:32:56 +0530

    [Deepanshu]: added Circle class with default and user-provided values

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Circle.java
@@ -0,0 +1,21 @@
+package com.constructor.level1;
+
+public class Circle {
+
+    private double radius;
+
+    // Default constructor
+    public Circle() {
+        this(1.0);   // constructor chaining
+    }
+
+    // Parameterized constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Method to display radius
+    public void displayRadius() {
+        System.out.println("Radius: " + radius);
+    }
+}
