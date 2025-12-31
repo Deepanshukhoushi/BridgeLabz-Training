9042446462d1a957a7de05c48d3b727b8a607385
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-12-31 22:35:57 +0530

    [Deepanshu]: added program of University with Faculties and Departments (Composition and Aggregation)

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/relationships/university/Department.java
@@ -0,0 +1,14 @@
+package com.relationships.university;
+
+public class Department {
+
+    private String departmentName;
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/relationships/university/Faculty.java
@@ -0,0 +1,14 @@
+package com.relationships.university;
+
+public class Faculty {
+
+    private String facultyName;
+
+    public Faculty(String facultyName) {
+        this.facultyName = facultyName;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+}
