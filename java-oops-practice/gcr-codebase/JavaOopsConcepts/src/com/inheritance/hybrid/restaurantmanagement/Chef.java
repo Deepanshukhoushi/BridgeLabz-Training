aa0b6c85d08b99c0733e85d6f2b94e621eadd47e
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-01 22:47:23 +0530

    [Deepanshu]: added restaurant management system using hybrid

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybrid/restaurantmanagement/Chef.java
@@ -0,0 +1,15 @@
+package com.inheritance.hybrid.restaurantmanagement;
+
+public class Chef extends Person implements Worker {
+
+    public Chef(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println("Chef Duties:");
+        System.out.println("Name : " + name);
+        System.out.println("Role : Cooking food and managing kitchen");
+    }
+}
