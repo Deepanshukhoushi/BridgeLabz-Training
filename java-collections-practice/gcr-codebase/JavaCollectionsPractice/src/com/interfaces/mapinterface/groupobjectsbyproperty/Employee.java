8a1b4ff91f21ce1f531d19a1dd09e55b5888753a
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-19 23:31:46 +0530

    [Deepanshu]: added program to group objects by property

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/interfaces/mapinterface/groupobjectsbyproperty/Employee.java
@@ -0,0 +1,23 @@
+package com.interfaces.mapinterface.groupobjectsbyproperty;
+class Employee {
+    private String name;
+    private String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
