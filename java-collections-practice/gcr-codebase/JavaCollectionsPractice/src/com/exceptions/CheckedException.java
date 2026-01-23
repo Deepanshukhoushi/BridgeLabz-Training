9c5cad2168ca416d712f156417bf70a248775afb
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-23 21:51:58 +0530

    [Deepanshu]: added program of Checked Exception (Compile-time Exception)

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exceptions/CheckedException.java
@@ -0,0 +1,21 @@
+package com.exceptions;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class CheckedException {
+    public static void main(String[] args) {
+
+        try (BufferedReader bufferedReader =
+                 new BufferedReader(new FileReader("data.txt"))) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
