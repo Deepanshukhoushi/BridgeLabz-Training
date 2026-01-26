576b76d15537e1df9189e7955a86886c8925d2c3
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-26 11:41:41 +0530

    [Deepanshu]: added program to Implement a Custom Caching System with @CacheResult

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotationsandreflection/annotations/customcatchingsystem/MathService.java
@@ -0,0 +1,17 @@
+package com.annotationsandreflection.annotations.customcatchingsystem;
+
+class MathService {
+
+    @CacheResult
+    public int slowSquare(int number) {
+
+        try {
+            Thread.sleep(2000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Computing square for: " + number);
+        return number * number;
+    }
+}
