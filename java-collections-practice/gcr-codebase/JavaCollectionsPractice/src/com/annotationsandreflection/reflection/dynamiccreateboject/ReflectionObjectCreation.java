9611d023f11d2f75f08352d31671c95504a0c082
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-27 16:17:33 +0530

    [Deepanshu]: added program of dynamic object creation

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotationsandreflection/reflection/dynamiccreateboject/ReflectionObjectCreation.java
@@ -0,0 +1,19 @@
+package com.annotationsandreflection.reflection.dynamiccreateboject;
+
+public class ReflectionObjectCreation {
+    public static void main(String[] args) {
+        try {
+            Class<?> studentClass = Class.forName("Student");
+
+            Object studentObj = studentClass.getDeclaredConstructor().newInstance();
+
+            Student student = (Student) studentObj;
+
+            student.display();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
