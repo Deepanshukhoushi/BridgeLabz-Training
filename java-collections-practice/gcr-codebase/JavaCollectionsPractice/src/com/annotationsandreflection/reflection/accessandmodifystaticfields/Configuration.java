a929b0bbe0108b5eeafca3e5bae37f89edc883d5
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-27 16:14:09 +0530

    [Deepanshu]: added program to access and modify static fields

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotationsandreflection/reflection/accessandmodifystaticfields/Configuration.java
@@ -0,0 +1,10 @@
+package com.annotationsandreflection.reflection.accessandmodifystaticfields;
+
+public class Configuration {
+
+    private static String API_KEY = "DEFAULT_KEY";
+
+    public static void printApiKey() {
+        System.out.println("API_KEY: " + API_KEY);
+    }
+}
