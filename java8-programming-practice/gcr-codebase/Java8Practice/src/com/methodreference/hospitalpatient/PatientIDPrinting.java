3c55dc0e3e905f4fbfc6d3174173ef19b346d67e
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-29 22:29:18 +0530

    [Deepanshu]: added program of patient id printing

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/methodreference/hospitalpatient/PatientIDPrinting.java
@@ -0,0 +1,25 @@
+package com.methodreference.hospitalpatient;
+
+import java.util.*;
+
+public class PatientIDPrinting {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        List<Integer> patientIds = new ArrayList<>();
+
+        System.out.print("Enter number of patients: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter patient IDs:");
+        for (int i = 0; i < n; i++) {
+            patientIds.add(sc.nextInt());
+        }
+
+        System.out.println("\nPatient IDs for Admin Verification:");
+        
+        patientIds.forEach(System.out::println);
+
+        sc.close();
+    }
+}
