a05c27270b3022e8e280d05618b4fbb23b7968e6
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-30 23:37:48 +0530

    [Deepanshu]: added program to find employees joined after 2015

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/organization/JoinedAfter2015.java
@@ -0,0 +1,11 @@
+package com.organization;
+
+public class JoinedAfter2015 {
+	public static void main(String[] args) {
+		EmployeeData.getEmployees().stream()
+        .filter(e -> e.getYearOfJoining() > 2015)
+        .map(Employee::getName)
+        .forEach(System.out::println);
+
+	}
+}
