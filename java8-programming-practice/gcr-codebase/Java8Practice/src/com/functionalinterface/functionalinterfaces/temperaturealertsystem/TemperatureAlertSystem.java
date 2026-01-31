d8522a41f7518b57b04a944e6a401c66859b1b0a
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-31 15:15:44 +0530

    [Deepanshu]: added program of temperature alert system

--- java8-programming-practice/gcr-codebase/Java8Practice/src/com/functionalinterface/functionalinterfaces/temperaturealertsystem/TemperatureAlertSystem.java
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/functionalinterface/functionalinterfaces/temperaturealertsystem/TemperatureAlertSystem.java
@@ -1,4 +1,4 @@
-package com.functionalinterface.temperaturealertsystem;
+package com.functionalinterface.functionalinterfaces.temperaturealertsystem;
 
 import java.util.Scanner;
 import java.util.function.Predicate;
