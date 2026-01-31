d894e7d69c31be72181c29929af3d3d888e6e74a
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-01-31 23:45:29 +0530

    [Deepanshu]: added program of date format utility

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/functionalinterface/staticmethods/dateformatutility/DateFormatUtils.java
@@ -0,0 +1,16 @@
+package com.functionalinterface.staticmethods.dateformatutility;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public interface DateFormatUtils {
+
+    static String formatDate(LocalDate date, String pattern) {
+
+        if (date == null || pattern == null) {
+            throw new IllegalArgumentException("Date or pattern cannot be null");
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return date.format(formatter);
+    }
+}
