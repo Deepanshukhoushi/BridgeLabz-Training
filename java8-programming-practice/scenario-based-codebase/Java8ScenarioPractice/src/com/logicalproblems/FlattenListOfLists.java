fcbe99f700accdbd3eb300afe33127e08d2475e4
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-02-01 23:03:53 +0530

    [Deepanshu]: added program of flattern list of lists

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/logicalproblems/FlattenListOfLists.java
@@ -0,0 +1,22 @@
+package com.logicalproblems;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlattenListOfLists {
+	public static void main(String[] args) {
+
+        List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5),
+                Arrays.asList(6, 7, 8)
+        );
+
+        List<Integer> result = listOfLists.stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+}
