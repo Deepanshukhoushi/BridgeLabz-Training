71df0cfc257fb3fd8d2873f8a74ea2cb4525f664
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-02-02 16:07:26 +0530

    [Deepanshu]: added program to convert string list to map

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/listmapconversion/StringListToMap.java
@@ -0,0 +1,29 @@
+package com.listmapconversion;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class StringListToMap {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter no. of Elements: ");
+		int n = input.nextInt();
+		List<String> str = new ArrayList<>();
+		System.out.println("Enter string: ");
+		for(int i = 0; i<n; i++) {
+			str.add(input.next());
+		}
+		
+		Map<String, Integer> map = str.stream().filter(Objects::nonNull).collect(Collectors.toMap(
+				e -> e, String::length, (existing, replacement) -> existing, 
+				LinkedHashMap::new));
+		
+		System.out.println(map);
+		input.close();
+	}
+}
