34db2691d702e7e4ce71c319ad813f4fb4314036
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-02-04 11:27:38 +0530

    [Deepanshu]: added program of User Creation (Factory)

---
+++ java-design-pattern-practice/gcr-codebase/JavaDesignPractice/src/com/designpattern/librarymanagementsystem/factory/Faculty.java
@@ -0,0 +1,8 @@
+package com.designpattern.librarymanagementsystem.factory;
+
+
+public class Faculty implements User {
+    public void showPrivileges() {
+        System.out.println("Faculty: Extended borrowing privileges");
+    }
+}
