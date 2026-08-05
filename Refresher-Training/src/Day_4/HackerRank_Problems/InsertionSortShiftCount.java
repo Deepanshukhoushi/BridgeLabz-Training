4a9c2f66a5f5f9110808a4f481e2fe9c40aab264
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-08-05 16:23:57 +0530

    [Deepanshu]: added program to count shifts in insertion sort

---
+++ Refresher-Training/src/Day_4/HackerRank_Problems/InsertionSortShiftCount.java
@@ -0,0 +1,31 @@
+package Day_4.HackerRank_Problems;
+
+import java.util.*;
+
+public class InsertionSortShiftCount {
+    public static int runningTime(int[] arr) {
+        int shifts = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];  // shift
+                shifts++;
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return shifts;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(runningTime(arr));
+        sc.close();
+    }
+}
