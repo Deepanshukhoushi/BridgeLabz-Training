ba1d9ae6c3d543daa0971ff96ef36edc962076f0
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-08-05 15:28:03 +0530

    [Deepanshu]: added program of post order traversal

--- Refresher-Training/src/Day_3/HackerRank_Problems/PostOrderTraversal.java
+++ Refresher-Training/src/Day_3/HackerRank_Problems/PostOrderTraversal.java
@@ -1,4 +1,75 @@
 package Day_3.HackerRank_Problems;
 
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class BinaryTree {
+
+    Node root;
+
+    // Insert into BST
+    Node insert(Node root, int data) {
+
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (data <= root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    // Postorder Traversal
+    void postOrder(Node root) {
+
+        if (root == null) {
+            return;
+        }
+
+        postOrder(root.left);
+
+        postOrder(root.right);
+
+        System.out.print(root.data + " ");
+    }
+}
+
 public class PostOrderTraversal {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        BinaryTree tree = new BinaryTree();
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            tree.root = tree.insert(tree.root, sc.nextInt());
+        }
+
+        System.out.println("Postorder Traversal:");
+
+        tree.postOrder(tree.root);
+
+        sc.close();
+    }
 }
